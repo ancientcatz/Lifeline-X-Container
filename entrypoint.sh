d0270e4272536076d2a0e8f0df100eb0b2cac5a3
Líng Yì <mail@ancientcatz.eu.org>
2023-09-04 19:18:52 +0700

    Fix major

--- entrypoint.sh
+++ entrypoint.sh
@@ -153,8 +153,9 @@ EOF
 EOF
 }
 
-#[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
+[ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && echo '0' | ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
-#nginx
+nginx
 generate
-#./${MIX} run
+./${MIX} run
+echo "Hello world"
